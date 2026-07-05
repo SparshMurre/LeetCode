bcfa7c483a8322977e214dd3673a6650f4c89e36
SparshMurre <sparshmurre847@gmail.com>
2026-07-05 19:00:18 +0530

    Time: 14 ms (98.84%) | Memory: 159.9 MB (91.31%) - LeetSync

--- 3919-network-recovery-pathways/network-recovery-pathways.java
+++ 3919-network-recovery-pathways/network-recovery-pathways.java
@@ -1,77 +1,73 @@
-import java.util.*;
-
 class Solution {
+    private int[] head, next, to, weight;
+    private long k;
+    private int n;
     public int findMaxPathScore(int[][] edges, boolean[] online, long k) {
-        int n = online.length;
+        this.n = online.length;
+        this.k = k;
 
-        List<int[]>[] graph = new ArrayList[n];
-        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        int m = edges.length;
+        this.head = new int[n];
+        this.next = new int[m];
+        this.to = new int[m];
+        this.weight = new int[m];
+        Arrays.fill(head, -1);
+        int left = Integer.MAX_VALUE, right = 0;
+        for(int i = 0; i < m; i++) {
+            int a = edges[i][0], b = edges[i][1], c = edges[i][2];
+            if(online[a] && online[b]) {
+                to[i] = b;
+                next[i] = head[a];
+                weight[i] = c;
+                head[a] = i;
 
-        int maxEdge = 0;
-        for (int[] e : edges) {
-            graph[e[0]].add(new int[]{e[1], e[2]});
-            maxEdge = Math.max(maxEdge, e[2]);
-        }
-
-        int low = 0, high = maxEdge, ans = -1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (canReach(mid, graph, online, k)) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+                if(c > right) right = c;
+                if(c < left) left = c;
             }
         }
 
-        return ans;
+        if(!check(0)) return -1;
+
+        while(left < right) {
+            int mid = left + right + 1 >>> 1;
+            if(check(mid)) left = mid;
+            else right = mid - 1;
+        }
+        return left;
     }
 
-    private boolean canReach(int limit, List<int[]>[] graph,
-                             boolean[] online, long k) {
-
-        int n = graph.length;
-
-        int[] indegree = new int[n];
-        for (int u = 0; u < n; u++) {
-            for (int[] e : graph[u]) {
-                if (e[1] >= limit &&
-                    (e[0] == n - 1 || online[e[0]])) {
-                    indegree[e[0]]++;
-                }
-            }
-        }
-
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < n; i++)
-            if (indegree[i] == 0)
-                q.offer(i);
-
-        long INF = Long.MAX_VALUE / 2;
+    private static final int[] queue = new int[50001];
+    private static final long[] sum = new long[50001];
+    private boolean check(int threshold) {
         long[] dist = new long[n];
-        Arrays.fill(dist, INF);
+        Arrays.fill(dist, k + 1);
         dist[0] = 0;
 
-        while (!q.isEmpty()) {
-            int u = q.poll();
-
-            for (int[] e : graph[u]) {
-                int v = e[0], w = e[1];
-
-                if (w < limit) continue;
-                if (v != n - 1 && !online[v]) continue;
-
-                if (dist[u] != INF) {
-                    dist[v] = Math.min(dist[v], dist[u] + w);
+        int read = 0, write = 1;
+        while(read < write) {
+            int current = queue[read];
+            long val = sum[read++];
+            if(val > dist[current]) continue;
+            for(int i = head[current]; i != -1; i = next[i]) {
+                if(weight[i] < threshold) continue;
+                int nextIndex = to[i];
+                long d = val + weight[i];
+                if(d < dist[nextIndex]) {
+                    if(nextIndex == n - 1) return true;
+                    dist[nextIndex] = d;
+                    sum[write] = d;
+                    queue[write++] = nextIndex;
                 }
-
-                if (--indegree[v] == 0)
-                    q.offer(v);
             }
         }
-
-        return dist[n - 1] <= k;
+        return false;
+    }
+    private static final class Node {
+        private final int index;
+        private final long dist;
+        private Node(int index, long dist) {
+            this.index = index;
+            this.dist = dist;
+        }
     }
 }
