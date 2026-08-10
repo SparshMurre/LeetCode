9f81688377694fdf356e600f633fa5808193f829
Sparsh Murre <sparshmurre847@gmail.com>
2026-08-10 19:49:46 +0530

    Time: 47 ms (42.19%) | Memory: 16.7 MB (42.05%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+vector<int> dp;
+bool solve(int n){
+    if (n==0)
+    return false;
+    if (dp[n] != -1)
+    return dp[n];
+    for (int i=1; i<= n / i; i++){
+        if (!solve(n-i*i)){
+            return dp[n]=1;
+        }
+    }
+    return dp[n]=0;
+}
+    bool winnerSquareGame(int n) {
+        dp.assign(n+1, -1);
+        return solve(n);        
+    }
+};
